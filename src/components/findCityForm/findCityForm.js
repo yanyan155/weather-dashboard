9fdf19ed56064cb3bb89cc177e425e0834a20094
yanyan155 <jm@tut.by>
2024-09-21 10:49:14 +0300

    feat: add cityName input validation

--- src/components/findCityForm/findCityForm.js
+++ src/components/findCityForm/findCityForm.js
@@ -1,11 +1,32 @@
 import { useEffect } from "react";
-import { Form, useLoaderData, useNavigation } from "react-router-dom";
+import {
+  Form,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+  watch,
+} from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { findUrl } from "../../utils/consts";
 import "./findCityForm.css";
 
 const FindCityForm = () => {
   const { cityName } = useLoaderData();
   const { location } = useNavigation();
+  const submit = useSubmit();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      cityName: cityName,
+    },
+    mode: "onChange",
+  });
+
+  console.log("errors", errors); // errors.cityName
 
   const searching =
     location && new URLSearchParams(location.search).has("cityName");
@@ -14,22 +35,46 @@ const FindCityForm = () => {
     document.getElementById("cityName").value = cityName;
   }, [cityName]);
 
+  const onSubmit = (data) =>
+    submit(data, { method: "get", action: `/${findUrl}` });
+
+  const isInputValid = errors?.cityName;
+
   return (
-    <Form method="get" action={`/${findUrl}`}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <h2 className="h4" id="cityFormTitle">
         Find a city
       </h2>
       <div className="position-relative">
+        <div id="search-spinner" className={searching ? "" : "d-none"} />
         <input
           type="text"
           name="cityName"
           id="cityName"
           placeholder="type city to search"
-          className={`form-control ${searching ? "loading" : ""}`}
-          defaultValue={cityName}
+          aria-invalid={isInputValid ? "true" : "false"}
+          className={`form-control ${searching ? "loading" : ""} ${
+            isInputValid ? "is-invalid" : ""
+          }`}
           aria-labelledby="cityFormTitle"
+          aria-describedby="empty-cityName wrong-symbol-cityName"
+          {...register("cityName", {
+            required: true,
+            pattern: {
+              value: /^[a-zA-Z0-9 -]*$/,
+            },
+          })}
         />
-        <div id="search-spinner" className={searching ? "" : "d-none"} />
+        {errors.cityName?.type === "required" && (
+          <div id="empty-cityName" className="form-text text-danger">
+            Please fill the input
+          </div>
+        )}
+        {errors.cityName?.type === "pattern" && (
+          <div id="wrong-symbol-cityName" className="form-text text-danger">
+            Only letters and numbers are allowed
+          </div>
+        )}
       </div>
       <button type="submit" className="btn btn-primary mt-3">
         Submit
